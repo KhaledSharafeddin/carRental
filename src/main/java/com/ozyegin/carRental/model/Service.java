b59bced99fdd600b8b1819e232d86933e4bf6874
KhaledSharafeddin <khaled.sh.02@gmail.com>
2024-11-23 17:56:44 +0300

    test

--- src/main/java/com/ozyegin/carRental/model/Service.java
+++ src/main/java/com/ozyegin/carRental/model/Service.java
@@ -1,9 +1,34 @@
 package com.ozyegin.carRental.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Service {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
 }
